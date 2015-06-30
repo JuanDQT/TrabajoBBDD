0353bf550844096f119cfa88522707dfd841d6b3
Juan Daniel Q <juandqt@gmail.com>
2015-06-30 19:23:37 +0200

    30/06 clase noche

--- src/main/java/demo/controllers/DeveloperController.java
+++ src/main/java/demo/controllers/DeveloperController.java
@@ -1,39 +1,36 @@
 package demo.controllers;
 
+import demo.exception.DeveloperException;
 import demo.model.Developer;
 import demo.repository.DeveloperRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /**
  * Created by poo2 on 29/06/2015.
  */
 @RestController
-@RequestMapping("/developers")//ruta de la web on estara el controller.
-
+@RequestMapping("/developers")
 public class DeveloperController {
 
-    @Autowired//conecta con el repositorio
+    @Autowired
     private DeveloperRepository developerRepository;
 
-    @RequestMapping(method = POST)//consultaa
-    @ResponseStatus(HttpStatus.CREATED)//crear
+
+    @RequestMapping(method= RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
     public Developer save(@RequestBody Developer developer) {
         return developerRepository.save(developer);
     }
 
-    @RequestMapping(method = GET)//read
+    @RequestMapping(method= RequestMethod.GET)
     public List<Developer> findAll() {
         List<Developer> developers = new ArrayList<Developer>();
         Iterator<Developer> iterator = developerRepository.findAll().iterator();
@@ -43,5 +40,36 @@ public class DeveloperController {
         }
 
         return developers;
+    }
+
+    @RequestMapping(value= "/{id}", method = GET)
+    public Developer getById(@PathVariable Long id) {
+        Developer developer = developerRepository.findOne(id);
+
+        if (developer == null)
+            throw new DeveloperException(id);
+
+        return developer;
+    }
+    @RequestMapping(value= "/{id}", method = RequestMethod.DELETE)
+         public Developer deleteById(@PathVariable Long id) {
+        Developer developer = developerRepository.findOne(id);
+
+        if (developer == null)
+            throw new DeveloperException(id);
+
+        developerRepository.delete(id);
+        return developer;
+    }
+
+    @RequestMapping(value= "/{id}", method = RequestMethod.PUT)
+    public Developer updateById(@PathVariable Long id, @RequestBody Developer newDeveloper) {
+        Developer developer = developerRepository.findOne(id);
+
+        if (developer == null)
+            throw new DeveloperException(id);
+
+        newDeveloper = developerRepository.save(newDeveloper);
+        return newDeveloper;
     }
 }
